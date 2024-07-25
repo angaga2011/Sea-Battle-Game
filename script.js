977c9d366c6b1599d1d5e3b50d181cda0f0631c7
Maksym Ostanin <144963945+angaga2011@users.noreply.github.com>
2024-07-25 00:17:43 -0400

    upd

--- script.js
+++ script.js
@@ -29,9 +29,20 @@ function startMulti(){
     let popUp = document.createElement("div");
     popUp.className = "popUp"
     document.body.appendChild(popUp);
-    let popUpCancelButton = document.createElement("button.popUpCancelButton")
+    let popUpCancelButton = document.createElement("button")
+    popUpCancelButton.className="popUpCancelButton";
     popUpCancelButton.innerText="X";
     popUp.appendChild(popUpCancelButton);
+
+    let gameNameInput = document.createElement("INPUT");
+    gameNameInput.className="input";
+    gameNameInput.setAttribute('type', 'text');
+    popUp.appendChild(gameNameInput);
+    let nicknameInput = document.createElement("input");
+    nicknameInput.className="input";
+    nicknameInput.setAttribute('type', 'text');
+    popUp.appendChild(nicknameInput);
+
     popUpCancelButton.addEventListener("click", function(){
         popUpCancelButton.remove();
         popUp.remove();
